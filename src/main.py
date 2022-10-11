6e3ed8dce4b6083e73ba0d003521715a80b73040
ThiccTheo <th3o1237@gmail.com>
2022-10-10 19:27:48 -0700

    sructure

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from urllib.request import HTTPCookieProcessor, build_opener
 from bs4 import BeautifulSoup
-from django.shortcuts import HttpResponse
 
 def main():
     opener = build_opener(HTTPCookieProcessor())
-    
-    with opener.open() as src:
+
+    with opener.open("https://www.swimcloud.com/swimmer/1644519/") as src:
+        webpage = src.read()
+
+    soup = BeautifulSoup(webpage, "html.parser")
+    title = soup.find("title")
+
+    with open("src/test.txt", 'w', encoding="UTF-8") as dst:
+        dst.write(title.text.strip())
 
 if __name__ == "__main__":
     main()
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{process::Command, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let cmd = "python3";
+    let script = "src/main.py";
+
+    Command::new(cmd)
+            .arg(script)
+            .spawn()
+            .expect("{cmd} command failed to start!");
+
+    let raw_contents = fs::read_to_string("src/test.txt")
+                        .expect("Could not open file!");
+
+    println!("{raw_contents}");
 }
