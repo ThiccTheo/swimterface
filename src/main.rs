c26b2f1a00dede72492ea0fc5f15aec84d57d740
ThiccTheo <th3o1237@gmail.com>
2022-10-10 21:29:25 -0700

    begun to integrate ggez

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
-use std::{process::Command, fs};
+use std::process::Command;
+use ggez::{self, ContextBuilder, event::{EventHandler, self}, GameError, Context, graphics::Rect};
+
+struct Game {}
+
+impl Game {
+    fn new(_context: &mut Context) -> Self {
+        Self {}
+    }
+}
+
+impl EventHandler for Game {
+    fn update(&mut self, _context: &mut ggez::Context) -> Result<(), GameError> {
+        Ok(())
+    }
+
+    fn draw(&mut self, _context: &mut ggez::Context) -> Result<(), GameError> {
+        let rect = Rect::new(0.0, 0.0, 20.0, 20.0);
+        Ok(())
+    }
+}
 
 fn main() {
     let cmd = "python3";
@@ -9,8 +29,15 @@ fn main() {
             .spawn()
             .expect("{cmd} command failed to start!");
 
-    let raw_contents = fs::read_to_string("src/test.txt")
-                        .expect("Could not open file!");
+    // let raw_contents = fs::read_to_string("src/test.txt")
+    //                     .expect("Could not open file!");
 
-    println!("{raw_contents}");
+    // println!("{raw_contents}");
+
+    let (mut context, event_loop) = ContextBuilder::new("Swimterface", "Theo Lee")
+                                .build()
+                                .expect("Failed to create application context!");
+
+    let game = Game::new(&mut context);
+    event::run(context, event_loop, game);
 }
